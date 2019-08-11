bdf1ba6453c0d4d191c3bd197377bcd4f3f20429
Luis Vinicius <viniborges@yahoo.com.br>
2019-08-11 19:20:27 -0300

    its a match - web

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -15,7 +15,16 @@
             }
 
             if (targetDev.likes.includes(loggedDev._id)) {
-                console.log('DEU MATCH');
+                const loggedSocket = req.connectedUsers[user];
+                const targetSocket = req.connectedUsers[devId];
+
+                if (loggedSocket) {
+                    req.io.to(loggedSocket).emit('match', targetDev);
+                }
+
+                if (targetSocket) {
+                    req.io.to(targetSocket).emit('match', loggedDev);
+                }
             }
 
             loggedDev.likes.push(targetDev._id);
--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import io from 'socket.io-client';
 import {Link} from 'react-router-dom';
 import './Main.css';
 
@@ -7,9 +8,11 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({match}){
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(() => {
         async function loadUsers() {
@@ -24,12 +27,23 @@ export default function Main({match}){
        loadUsers(); 
     }, [match.params.id]);
 
+    useEffect(() => {
+        const socket = io('http://localhost:3333', {
+            query: { user: match.params.id}
+        });
+
+        socket.on('match', dev => {
+            setMatchDev(dev);
+        })
+     
+    }, [match.params.id])
+
     async function handleLike(id) {
         await api.post(`/devs/${id}/likes`,null,{
             headers: { user: match.params.id}
         });
 
-        setUsers(users.filter(user => user._id != id));
+        setUsers(users.filter(user => user._id !== id));
     }
 
     async function handleDislike(id) {
@@ -37,7 +51,7 @@ export default function Main({match}){
             headers: { user: match.params.id}
         });
 
-        setUsers(users.filter(user => user._id != id));
+        setUsers(users.filter(user => user._id !== id));
     }
     
     return (
@@ -69,6 +83,17 @@ export default function Main({match}){
             ) : (
                 <div className="empty"> Acabou :( </div>
             ) }
+
+            { matchDev && (
+                <div className="match-container">
+                    <img src={itsamatch} alt="It's a match"/>
+                    <img className="avatar" src={matchDev.avatar} alt="" />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+
+                    <button type="button" onClick={() => setMatchDev(null)} >Fechar</button>
+                </div>
+            )}
         </div>
     )
 }
